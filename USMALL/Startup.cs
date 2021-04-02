5fe0717c35cef8cee3b0b6b1a8acabca8a57c2d0
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-02 17:16:23 +0500

    Added  AddControllersWithViews method in the ConfigureServices method to set up shared objects requiered by applications using MVC framework and Razor view engine

--- USMALL/Startup.cs
+++ USMALL/Startup.cs
@@ -16,24 +16,21 @@ namespace USMALL
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
