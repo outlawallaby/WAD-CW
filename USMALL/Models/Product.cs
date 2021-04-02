5fe954fa0c910f698cdc369addff8be81e7d18b1
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-02 20:36:34 +0500

    Comments added to Product class fixed ViewStart files content

--- USMALL/Models/Product.cs
+++ USMALL/Models/Product.cs
@@ -11,6 +11,8 @@ namespace USMALL.Models
         public long ProductID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        //SQL data type that will be used to store values for Price property 
         [Column(TypeName = "decimal(8,2)")]
         public decimal Price { get; set; }
 
