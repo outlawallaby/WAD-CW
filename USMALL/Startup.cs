386238001945eece5b1403396291fde7a4f60758
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-02 21:41:04 +0500

    I implemented AddDbContext method inside Startup which register the database context class and configures the relationship with the database UseSqlServer method used declare SQL server and Connection string is read via IConfiguration Object

--- USMALL/Startup.cs
+++ USMALL/Startup.cs
@@ -7,16 +7,31 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using USMALL.DAL;
+using USMALL.Models;
 
 namespace USMALL
 {
     public class Startup
     {
+        public  Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        private IConfiguration Configuration { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opts =>
+            {
+                opts.UseSqlServer(
+                    Configuration["ConnectionString:USMALLConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
