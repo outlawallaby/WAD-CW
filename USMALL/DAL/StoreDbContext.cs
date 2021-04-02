75c84cf5bbe5e9cdd540fc37e764d240adbc7e86
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-02 21:33:10 +0500

    In oreder to provide access to the database through a context class which i implemented StoreDbContext to DAL folder

---
+++ USMALL/DAL/StoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using USMALL.Models;
+
+namespace USMALL.DAL
+{
+    public class StoreDbContext:DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext>options)
+            : base(options) { }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
