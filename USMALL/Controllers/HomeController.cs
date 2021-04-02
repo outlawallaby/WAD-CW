c2e2940b786c72c60b27702e8224bed95534626c
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-02 22:16:14 +0500

    Dependency injection, and its approach allows the HomeController object to access the application’s repository through the IStoreRepository interface without knowing which implementation class has been configured

--- USMALL/Controllers/HomeController.cs
+++ USMALL/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using USMALL.Models;
+using USMALL.DAL;
 
 namespace USMALL.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index() => View(repository.Products);
     }
 }
