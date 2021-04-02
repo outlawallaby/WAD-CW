8691f239b530afe654d5c09655f3132a8053aeed
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-02 23:38:29 +0500

    ASP.NET routing feature makes it easy to change URL scheme in application
    I added a new route in the Startup class

--- USMALL/Startup.cs
+++ USMALL/Startup.cs
@@ -44,8 +44,10 @@ namespace USMALL
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("pagination",
+                    "Products/Page{productPage}",
+                new { Controller = "Home", action = "Index" });
                 endpoints.MapDefaultControllerRoute();
             });
             SeedData.EnsurePopulated(app);
