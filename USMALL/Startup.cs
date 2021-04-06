2d9aa2863961650f50210dd9a084d4ecd40f3f97
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-06 22:25:26 +0500

    AddScoped method specifies that the same object should be used to satisfy related requests for Cart instances. How requests are related can be configured, but by default, it means that any Cart required by components handling the same HTTP request will receive the same object.
    AddSingleton method, which specifies that the same object should always be used. Created service tells ASP.NET Core to use the HttpContextAccessor class when implementations of the IHttpContextAccessor interface
    are required. This service is required so I can access the current session in the SessionCart class.

--- USMALL/Startup.cs
+++ USMALL/Startup.cs
@@ -36,6 +36,8 @@ namespace USMALL
             services.AddRazorPages();
             services.AddDistributedMemoryCache();
             services.AddSession();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
