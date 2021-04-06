e3ecb64fee5b7f5be4686505d2ccf3ae609d0fb8
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-06 19:30:54 +0500

    I removed the placeholder text and replaced it with the vc:navigation-menu element,which inserts the view component.The element omits the ViewComponent part of the class name and hyphenates it,such that vc:navigation-menu specifies the NavigationMenuViewComponent class
    Added reference to USMALL.DAL inside NavigationMenuViewComponent

--- USMALL/Components/NavigationMenuViewComponent.cs
+++ USMALL/Components/NavigationMenuViewComponent.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using USMALL.DAL;
 
 namespace USMALL.Components
 {
-    public class NavigationMenuViewComponent:ViewComponent
+    public class NavigationMenuViewComponent : ViewComponent
     {
-        public string Invoke() {
-            return "Hello from the Nav view Component"
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
         }
     }
 }
