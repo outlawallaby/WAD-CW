1391e438b006113ee6935d23257c0b0789e3d1a6
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-06 22:30:24 +0500

    The page model class indicates that it needs a Cart object by declaring a constructor argument, which has allowed application to remove the statements that load and store sessions from the handler methods.

--- USMALL/Pages/Cart.cshtml.cs
+++ USMALL/Pages/Cart.cshtml.cs
@@ -12,9 +12,10 @@ namespace USMALL.Pages
     public class CartModel : PageModel
     {
         private IStoreRepository repository;
-        public CartModel(IStoreRepository repo)
+        public CartModel(IStoreRepository repo, Cart cartService)
         {
             repository = repo;
+            Cart = cartService;
         }
         public Cart Cart { get; set; }
         public string ReturnUrl { get; set; }
@@ -26,15 +27,13 @@ namespace USMALL.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            
         }
         public IActionResult OnPost(long productId, string returnUrl)
         {
             Product product = repository.Products
             .FirstOrDefault(p => p.ProductID == productId);
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             Cart.AddItem(product, 1);
-            HttpContext.Session.SetJson("cart", Cart);
             return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
