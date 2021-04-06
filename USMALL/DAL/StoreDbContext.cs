7993001b1f12c3afc844cd852afb6404cc827e4b
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-06 23:31:33 +0500

    Added Orders model to database
    Create migration using command
    dotnet ef migrations add Order
    inside project folder

--- USMALL/DAL/StoreDbContext.cs
+++ USMALL/DAL/StoreDbContext.cs
@@ -12,6 +12,6 @@ namespace USMALL.DAL
         public StoreDbContext(DbContextOptions<StoreDbContext>options)
             : base(options) { }
         public DbSet<Product> Products { get; set; }
-
+        public DbSet<Order> Orders { get; set; }
     }
 }
