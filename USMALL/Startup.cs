65da2eedbacb0eb485ba2bcbdbe298124fff0726
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-06 18:48:32 +0500

    Refining the URL Scheme
    /Category/Page№

--- USMALL/Startup.cs
+++ USMALL/Startup.cs
@@ -45,9 +45,19 @@ namespace USMALL
             app.UseRouting();
 
             app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("catpage",
+                    "{category}/Page{productPage:int}",
+                    new { Controller = "Home", action = "Index" });
+               
+                endpoints.MapControllerRoute("page", "Page{productPage:int}",
+                    new { Controller = "Home", action = "Index", productPage = 1 });
+            
+                endpoints.MapControllerRoute("category", "{category}",
+                    new { Controller = "Home", action = "Index", productPage = 1 });
+                
                 endpoints.MapControllerRoute("pagination",
-                    "Products/Page{productPage}",
-                new { Controller = "Home", action = "Index" });
+                     "Products/Page{productPage}",
+                     new { Controller = "Home", action = "Index", productPage = 1 });
                 endpoints.MapDefaultControllerRoute();
             });
             SeedData.EnsurePopulated(app);
