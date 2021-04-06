4fa4d3bd970323b9a90ac9c19fde0a42f69ee3ed
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-06 21:03:50 +0500

    Correct the page links so that they work correctly when a category is selected. Updated the Index action method in HomeController so that the pagination inforamtion takes the categories into account

--- USMALL/Controllers/HomeController.cs
+++ USMALL/Controllers/HomeController.cs
@@ -26,7 +26,10 @@ namespace USMALL.Controllers
             {
                 CurrentPage = productPage,
                 ItemsPerPage = PageSize,
-                TotalItems = repository.Products.Count()
+                TotalItems = category ==null ?
+                    repository.Products.Count() :
+                    repository.Products.Where(e=>
+                        e.Category==category).Count()
             }
         });
 
