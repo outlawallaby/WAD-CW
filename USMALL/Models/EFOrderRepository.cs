a3fba4a572ae943f1dd7442913a39fa761661e23
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-06 23:36:06 +0500

    This class implements the IOrderRepository interface using Entity Framework Core, allowing the set of Order objects that have been stored to be retrieved and allowing for orders to be created or changed.

---
+++ USMALL/Models/EFOrderRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using USMALL.DAL;
+
+namespace USMALL.Models
+{
+    public class EFOrderRepository
+    {
+        private StoreDbContext context;
+        public EFOrderRepository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Order> Orders => context.Orders
+         .Include(o => o.Lines)
+        .ThenInclude(l => l.Product);
+        public void SaveOrder(Order order)
+        {
+            context.AttachRange(order.Lines.Select(l => l.Product));
+            if (order.OrderID == 0)
+            {
+                context.Orders.Add(order);
+            }
+            context.SaveChanges();
+
+        }
+    }
+}
