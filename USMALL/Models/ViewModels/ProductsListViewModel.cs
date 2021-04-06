367a9eea9289fd72f664b029ebcfc8f2c5d4a300
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-06 18:09:09 +0500

    Property called CurrentCategory to indicate which category has been selected

--- USMALL/Models/ViewModels/ProductsListViewModel.cs
+++ USMALL/Models/ViewModels/ProductsListViewModel.cs
@@ -10,5 +10,6 @@ namespace USMALL.Models.ViewModels
     {
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
