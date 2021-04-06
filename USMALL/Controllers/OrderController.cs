80443368294a6352bd4b1132c5fdde26615769ba
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-06 23:20:25 +0500

    Checkout method returns the default view and passes a new Order object as the view model.

---
+++ USMALL/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using USMALL.Models;
+using USMALL.DAL;
+
+namespace USMALL.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
