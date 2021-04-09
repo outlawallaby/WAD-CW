8e507d3cfaca15137af615444b26e2a06cf4a9e0
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-09 16:21:48 +0500

    Implemented Repository interfaces

--- USMALL/Models/EFStoreRepository.cs
+++ USMALL/Models/EFStoreRepository.cs
@@ -15,5 +15,22 @@ namespace USMALL.Models
             context = ctx;
         }
         public IQueryable<Product> Products => context.Products;
+
+        public void CreateProduct(Product p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(Product p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+        }
+
+        public void SaveProduct(Product p)
+        {
+            context.SaveChanges();
+        }
     }
 }
