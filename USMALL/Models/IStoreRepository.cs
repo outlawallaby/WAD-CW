b4649cef0512e3d441ae0be6683cf3bcb334fa70
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-09 16:19:53 +0500

    Added CRUD features to the repository

--- USMALL/Models/IStoreRepository.cs
+++ USMALL/Models/IStoreRepository.cs
@@ -8,5 +8,9 @@ namespace USMALL.Models
     public interface IStoreRepository
     {
         IQueryable<Product> Products { get; }
+
+        void SaveProduct(Product p);
+        void CreateProduct(Product p);
+        void DeleteProduct(Product p);
     }
 }
