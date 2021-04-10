2d3d45f03dc5742c45194079bb2c5c5ba36c1f71
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-10 14:57:11 +0500

    This code ensures the database is created and up-to-date and uses the UserManager<T> class, which is provided as a service by ASP.NET Core Identity for managing users. The database is searched for the Admin user account, which is created—with a password of Ovosikov8487$—if it is not present.

---
+++ USMALL/DAL/IdentitySeedData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using USMALL.Models;
+
+namespace USMALL.DAL
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "00008487";
+        private const string adminPassword = "Ovosikov8487$";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            AppIdentityDbContext context = app.ApplicationServices
+            .CreateScope().ServiceProvider
+            .GetRequiredService<AppIdentityDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            UserManager<IdentityUser> userManager = app.ApplicationServices
+            .CreateScope().ServiceProvider
+            .GetRequiredService<UserManager<IdentityUser>>();
+            IdentityUser user = await userManager.FindByIdAsync(adminUser);
+            if (user == null)
+            {
+                user = new IdentityUser("00008487");
+                user.Email = "ovosikov@students.wiut.uz";
+                user.PhoneNumber = "90-9663633";
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
