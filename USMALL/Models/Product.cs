bfc0785e156a25376d999947aaed22c8e018f938
Otabek Vosikov <vosiqov18.o@gmail.com>
2021-04-10 14:05:45 +0500

    Validate the values the user provides when editing or creating Product objects.

--- USMALL/Models/Product.cs
+++ USMALL/Models/Product.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace USMALL.Models
 {
     public class Product
     {
         public long ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product description")]
         public string Description { get; set; }
 
+        [Required]
+        [Range(0.01, double.MaxValue,
+            ErrorMessage = "Please enter a positive price")]
         //SQL data type that will be used to store values for Price property 
         [Column(TypeName = "decimal(8,2)")]
         public decimal Price { get; set; }
-
+        
+        [Required(ErrorMessage ="Please specify a category")]
         public string Category { get; set; }
     }
 }
